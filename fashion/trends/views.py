bc8676f79c91ca90c6aa56180bf233901b36c866
Yauheniya <e-zabotina@inbox.ru>
2023-02-14 13:37:03 +0300

    study project 1

--- fashion/trends/views.py
+++ fashion/trends/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from .models import *
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 def index(request):
     posts = Trends.objects.all()
-    return render(request, 'trends/index.html', {'posts': posts},)
+    return render(request, 'trends/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
 
 def about(request):
-    return render(request, 'trends/about.html',{'title': 'О сайте'})
+    return render(request, 'trends/about.html',{'menu': menu, 'title': 'О сайте'})
 
 def categ(request, categ):
     if request.POST:
