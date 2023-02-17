4ffd3ff4fe4cc0f46debd6bd350606be9a60a3bf
Yauheniya <e-zabotina@inbox.ru>
2023-02-17 15:33:17 +0300

    study project 1

--- fashion/trends/admin.py
+++ fashion/trends/admin.py
@@ -7,8 +7,16 @@ class TrendsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
 
 admin.site.register(Trends, TrendsAdmin)
-
+admin.site.register(Category, CategoryAdmin)
 
 # Register your models here.
--- fashion/trends/models.py
+++ fashion/trends/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 class Trends (models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%n/%d/')
+    photo = models.ImageField(upload_to='photos/%Y/')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
